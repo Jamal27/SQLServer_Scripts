a8a39e98cc52a4651bb201086829d2ee0dd113c1
Reginaldo Silva <reginaldo.silva27@gmail.com>
2018-08-31 12:46:24 -0300

    Update Test-DbaLastbackup.ps1

--- Test-DbaLastbackup.ps1
+++ Test-DbaLastbackup.ps1
@@ -4,14 +4,32 @@
 $cred = Import-Clixml C:\Temp\cred.xml 
 
 ##Executando Teste de Backup e salvando resultado em variavel
-$ResultTestBackup = Test-DbaLastBackup -SqlServer DESKTOP-A7S2JPV\SQLSERVER2016 -Databases SQLDAYES -WarningVariable Warnings 
+$ResultTestBackup = Test-DbaLastBackup -SqlServer DESKTOP-A7S2JPV\SQLSERVER2016 -Database SQLDAYES,master -WarningVariable Warnings -IgnoreLogBackup
 
 ##Exportando resultado para arquivo .txt que será enviado por e-mail
 $ResultTestBackup > 'C:\temp\ResultEmail.txt'
 $Warnings >>  'C:\temp\ResultEmail.txt'
 
-##Convertendo resultado para o padrão HTML no formato de lista, adicionando uma Header utilizando HTML
-$ResultTestHtml = ConvertTo-Html -InputObject $ResultTestBackup -As List -Head '<h1 style="color:blue;">Test Last Backup: SUCCEED</h1>'
+##Declarando e zerando a variável
+$ResultTestHtml = ""
+
+##variável de controle
+$count = 0
+
+ForEach($obj in $ResultTestBackup)
+{   
+    if($count.Equals(0))
+    {     
+        $ResultTestHtml +=  ConvertTo-Html -InputObject $obj -As List -Head '<h1 style="color:blue;">Test Last Backup: SUCCEED</h1>'         
+    }
+    else
+    {
+        $ResultTestHtml += "<br></br>"        
+        $ResultTestHtml +=  ConvertTo-Html -InputObject $obj -As List 
+    }    
+    $count +=1
+}
+
 
 ##Convertendo para string para ser enviado ao parametro -Body do comando Send-MailMessage, retirando informações irrelevantes 
 $ResultTestHtml = [string]$ResultTestHtml.Replace('<tr><td>BackupDate:</td><td>System.Object[]</td></tr>','').Replace('<tr><td>BackupFiles:</td><td>System.Object[]</td></tr>','')
@@ -24,4 +42,5 @@ if($ResultTestHtml.Contains("Failure") -or $ResultTestHtml.Contains("error"))
 
 ##Envia E-mail com arquivo .txt em anexo e corpo HTML
 Send-MailMessage -Credential $cred -SmtpServer "smtp.office365.com" -To "reginaldo.silva@dataside.com.br" -From "reginaldo.silva@dataside.com.br" -Subject "Test Last Backup - Dbatools" -UseSsl -Body $ResultTestHtml -BodyAsHtml -Attachments 'C:\temp\ResultEmail.txt' -Priority High
+
 
