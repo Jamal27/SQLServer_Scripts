e7d78c71c6f50dcd2bdd226989546c666304dbbe
Reginaldo Silva <reginaldo.silva27@gmail.com>
2018-09-12 01:08:02 -0300

    Update CallSQLVersion.ps1

--- CallSQLVersion.ps1
+++ CallSQLVersion.ps1
@@ -1,15 +1,15 @@
 ﻿#Load SMO
 [Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")  
 
-#navigate to folder
+#navigate to folder that you save files
 Set-Location C:\temp\
 
-#Load function Get-SQLBuildVersion to memory of this session
+#Load function Get-SQLBuildVersion to memory of this powershell session
 . .\Get-SQLBuildVersion.ps1
 
-#Import your email credential
+#Import your email credential, you can export using this command: $cred = Import-Clixml C:\Temp\cred.xml, put yours credentials on the pop-up and save it.
 $cred = Import-Clixml C:\Temp\cred.xml 
 
 #Call function with your parameters
-Get-SQLBuildVersion -SqlInstance "DESKTOP-A7S2JPV\SQLSERVER2016","DESKTOP-A7S2JPV\SQLSERVER2014" -SmtpServer "smtp.office365.com" -EmailFrom "reginaldo.silva@dataside.com.br" -EmailTo "reginaldo.silva@dataside.com.br" -CredentialEmail $cred 
+Get-SQLBuildVersion -SqlInstance "DESKTOP-A7S2JPV\SQLSERVER2016","DESKTOP-A7S2JPV\SQLSERVER2014" -SmtpServer "smtp.gmail.com" -EmailFrom "reginaldo.silva27@gmail.com" -EmailTo "reginaldo.silva27@gmail.com" -CredentialEmail $cred 
 
