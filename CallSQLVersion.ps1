7f191ea5a223909c44750d24cb6dc557a9a51b48
Reginaldo Silva <reginaldo.silva27@gmail.com>
2018-09-12 02:20:21 -0300

    Update CallSQLVersion.ps1

--- CallSQLVersion.ps1
+++ CallSQLVersion.ps1
@@ -8,8 +8,10 @@ Set-Location C:\temp\
 . .\Get-SQLBuildVersion.ps1
 
 #Import your email credential, you can export using this command: Get-Credential | Export-Clixml C:\Temp\credemail.xml, put yours credentials on the pop-up and save it.
+####### ALTER HERE #######
 $cred = Import-Clixml C:\Temp\cred.xml 
 
 #Call function with your parameters
+####### ALTER HERE #######
 Get-SQLBuildVersion -SqlInstance "DESKTOP-A7S2JPV\SQLSERVER2016","DESKTOP-A7S2JPV\SQLSERVER2014" -SmtpServer "smtp.gmail.com" -EmailFrom "reginaldo.silva27@gmail.com" -EmailTo "reginaldo.silva27@gmail.com" -CredentialEmail $cred 
 
